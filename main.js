ce9f78d8bccf71fe277d888bb15be438855980da
olympia <owojcik7@gmail.com>
2019-10-23 14:56:57 -0400

    Generate random password function

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const clipboardEl = document.getElementById("clipboard")
 //pass in event listener type and functino to run when listener goes off
 generateEl.addEventListener('click', () => {
     const length = +lengthEl.value
-    console.log(typeof length) //string, I want this to be a # so add a plus sign to make it a number, urnary operator
+    // console.log(typeof length) //string, I want this to be a # so add a plus sign to make it a number, urnary operator
 
     //see if values are checked or not, the checked property is true or false
     const hasLower = lowercaseEl.checked;
@@ -30,10 +30,44 @@ generateEl.addEventListener('click', () => {
 //Step 5: GeneratePassword function
 function generatePassword(lower, upper, number, symbol, length) {
     //1. Init a password variable
-    const password = ""
+    let generatedPassword = ""
     //2. Filter out un-checked types, if not checked, don't incude
+    const typesCount = lower + upper + number + symbol;
+    let typesArr = [{ lower }, { upper }, { number }, { symbol }]; //array of objects, lower:true, upper:false
+    console.log('typesArr', typesArr)
+    //filter unchecked, loop through each item and based on true or false value, filter out anything that equals false - so we need to actually get value of true or false
+    //pass in item, an array, and we want the first value 0 - so if it's false, should be filtered out of array
+    typesArr = typesArr.filter((item) => {
+        console.log('item', Object.values(item))
+        return Object.values(item)[0]
+    })
+
+    //if there's none checked, don't proceed
+    if (typesCount === 0) {
+        return "";
+    }
+
     //3. Loop over the length, w/e the length is, we wanna loop over and call a generator function for each type
-    //4 Add final password to the password variable and return
+    //for each true in the array, return an array of keys, and grab the first item in that
+
+    for (let i = 0; i < length; i += typesCount) {
+        //for each object in this array {number:true}, get the key of this object and return in an array
+        typesArr.forEach(type => {
+            //funcName is either upper, lower number or sumbol
+            const funcName = Object.keys(type)[0]
+            console.log(funcName)
+            //append to generated password
+            generatedPassword += randomFunc[funcName]();
+
+        })
+
+    }
+
+
+    //4 Add final password to the password variable and return, 
+    //if I put 1 in the password lenght, this is still 4. We need to slice it by length.
+    const finalPassword = generatedPassword.slice(0, length)
+    return finalPassword
 
 }
 
@@ -64,19 +98,17 @@ function getRandomUpper() {
 
 function getRandomNumber() {
     //48 to 57, a span of 10
-
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
 
 function getRandomSymbol() {
     const symbols = "!@#$%^&*(){}+/,."
-
     return symbols[Math.floor(Math.random() * symbols.length)]
 }
 
 
 
-console.log(getRandomLower())
-console.log(getRandomUpper())
-console.log(getRandomNumber())
-console.log(getRandomSymbol())
+// console.log(getRandomLower())
+// console.log(getRandomUpper())
+// console.log(getRandomNumber())
+// console.log(getRandomSymbol())
