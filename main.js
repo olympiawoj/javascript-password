2e9605594ab8f3eef486026dfe870d63896b559e
olympia <owojcik7@gmail.com>
2019-10-23 13:53:19 -0400

    Select DOM elements and add event listener to generate btn

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+
+//Step 3: Select all DOM elements that we need
+//get result, span with id of result
+//Ctrl D to select next instance
+const resultEl = document.getElementById("result")
+const lengthEl = document.getElementById("length")
+const uppercaseEl = document.getElementById("uppercase")
+const lowercaseEl = document.getElementById("lowercase")
+const numbersEl = document.getElementById("numbers")
+const symbolsEl = document.getElementById("symbols")
+const generateEl = document.getElementById("generate")
+const clipboardEl = document.getElementById("clipboard")
+
+//Step 4: Think about events - 2 events, click generate pass button and click clipboard
+//pass in event listener type and functino to run when listener goes off
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value
+    console.log(typeof length) //string, I want this to be a # so add a plus sign to make it a number, urnary operator
+
+    //see if values are checked or not, the checked property is true or false
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbols.checked;
+    //pass all of these into a function called generate password
+    //result will get put into the result element
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
+});
+
+//Step 5: GeneratePassword function
+function generatePassword(lower, upper, number, symbol, length) {
+    //1. Init a password variable
+    const password = ""
+    //2. Filter out un-checked types, if not checked, don't incude
+    //3. Loop over the length, w/e the length is, we wanna loop over and call a generator function for each type
+    //4 Add final password to the password variable and return
+
+}
+
+
+//Step 2: Object of diff functions
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
 //Step 1: Four Generator Functions - http://www.net-comber/charset.html
 
 function getRandomLower() {
